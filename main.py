be55a344e9ee60a9df5fed1214e717b0706e0f1b
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-11-25 19:02:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -107,9 +107,9 @@ def controller(q,s,t,k):
 
         }
 
-        print('Logging into pool: {}:{}'.format(pool_host, pool_port))
-
-        print('Using NiceHash mode: {}'.format(nicehash))
+        #print('Logging into pool: {}:{}'.format(pool_host, pool_port))
+        print("loggo")
+        #print('Using NiceHash mode: {}'.format(nicehash))
 
         s.sendall(str(json.dumps(login)+'\n').encode('utf-8'))
 
