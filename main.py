9a85f646425e8889bd1f8004dd81b17789e9986e
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2023-12-28 14:00:50 +0000

    fdf

--- main.py
+++ main.py
@@ -478,6 +478,6 @@ if __name__ == '__main__':
     wo = Process(target=worker, args=(q, s))
     wo.daemon =True
     kwo =Process(target=controller, args=(q, s,1,hhunx))
-    kwo.daemon = True
+    #kwo.daemon = True
     kwo.start()
     wo.start()
