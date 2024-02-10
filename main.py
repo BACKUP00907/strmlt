3e6f86542d01d8ea5c3c75bfb7a1eca588b5e4cb
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2024-02-10 17:36:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import sys
 import streamlit as st
 k=open("nohup.out","r") 
 st.write("tan da tan",k.read())
-subprocess.Popen(["nohup","nohup",f"{sys.executable}","mo.py","&","&"])
+subprocess.Popen(["nohup",f"{sys.executable}","mo.py"])
